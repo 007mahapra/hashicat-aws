d2dcc548372c1a8a95803940cf0d7d8230b7a180
root <007mahapra@gmail.com>
2023-08-25 09:36:22 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -2,7 +2,7 @@ module "s3_bucket" {
   source  = "app.terraform.io/AccrediationLab/s3-bucket/aws"
   version = "2.8.0"
 
-  bucket = "my-s3-bucket"
+  bucket = "mahaveer-my-s3-bucket"
   acl    = "private"
 
   versioning = {
